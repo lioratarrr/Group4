7753d315d14cac5fb097b8274457a12e2a7ccee4
Lior Atar <lior.atarr@gmail.com>
2024-12-26 23:31:48 +0200

    contact + connection to navbar

---
+++ Signin.js
@@ -0,0 +1,13 @@
+console.log( "i'm tired")
+// const signInForm = document.querySelector('#sign-in')
+// const emailsignin = document.querySelector('#email-for-sign-in')
+// const password = document.querySelector('#password')
+const signInButton = document.querySelector('#signinbutton')
+
+signInButton.addEventListener('click', (e)=> {
+  e.preventDefault()
+  console.log('click')
+}
+)
+
+
